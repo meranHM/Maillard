7760309b250e98dc9d127f119f3b6f2d0bb49cd8
pikatak1381-MSH <pikatak1381@gmail.com>
2025-09-21 21:31:05 +0330

    Update: move mobile drawer state to zustand ui store, create global state for angular gradiant background

--- store/uiStore.ts
+++ store/uiStore.ts
@@ -1,2 +1,32 @@
 // UI Zustand states (modals, Hamburgur menus, theme)
 import { create } from "zustand"
+
+interface uiStoreState {
+    mobileNavbarOpen: boolean
+    toggleMobileNavbar: () => void
+    index: number
+    angle: number
+    next: () => void
+    prev: () => void
+}
+
+export const useUiStore = create<uiStoreState>((set) => ({
+    index: 0,
+    angle: 0,
+    mobileNavbarOpen: false,
+
+    toggleMobileNavbar: () => 
+        set((state) => ({ mobileNavbarOpen: !state.mobileNavbarOpen })),
+
+    next: () =>
+        set((state) => ({
+            index: state.index + 1,
+            angle: state.angle + 45,
+        })),
+    
+    prev: () =>
+        set((state) => ({
+            index: state.index - 1,
+            angle: state.angle - 45,
+        })),
+}))
