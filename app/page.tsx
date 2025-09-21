504bab0ce6e3de608f41d730ebf32d6b3b84bf6b
pikatak1381-MSH <pikatak1381@gmail.com>
2025-09-21 21:32:00 +0330

    Add: shadcn dialog, reusable Animated Container, rotating angular Gradient Background components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+
+import GradientBackground from "@/components/ui/GradientBackground"
 import Image from "next/image"
+import { useUiStore } from "@/store/uiStore"
 
 export default function Home() {
+  const { next, prev } = useUiStore()
+
   return (
-    <div className="">
-      Home
+    <div 
+      className="h-screen flex flex-col items-center justify-center"
+    >
+      <GradientBackground />
+      <button onClick={next} className="p-4 cursor-pointer">Next</button>
+
+      <button onClick={prev} className="p-4 cursor-pointer">Prev</button>
     </div>
   )
 }
