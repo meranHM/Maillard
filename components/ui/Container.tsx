66f651f4773d10909d6ba9660016803136705bf2
pikatak1381-MSH <pikatak1381@gmail.com>
2025-09-23 21:20:58 +0330

    Add: Reusable container component

---
+++ components/ui/Container.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils"
+import { HTMLAttributes } from "react"
+
+type ContainerProps = HTMLAttributes<HTMLDivElement>
+
+const Container: React.FC<ContainerProps> = ({ 
+    className, 
+    children,
+    ...props
+}) => {
+  return (
+    <div
+        className={cn(
+            "container flex flex-col items-center justify-center gap-10 px-6.75 py-4.75 mt-6.25",
+            className
+        )}
+        {...props}
+    >
+        {children}
+    </div>
+  )
+}
+
+export default Container
