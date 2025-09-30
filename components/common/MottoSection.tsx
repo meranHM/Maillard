34a88dee97a042bbea26ee580ba2e6eea63f870c
pikatak1381-MSH <pikatak1381@gmail.com>
2025-09-30 19:53:06 +0330

    Update: make motto section responsive for desktop view

--- components/common/MottoSection.tsx
+++ components/common/MottoSection.tsx
@@ -8,6 +8,7 @@ const MottoSection = () => {
     <Container
       className="relative w-full max-w-7xl h-[500px] bg-gradient-to-b from-[#FAFAFA] via-[#FFFFFF00] to-[#F0F0F0]"
     >
+      {/* Underlay */}
       <div
         className="z-0 absolute w-full max-w-[371px] h-auto"
       >
@@ -21,20 +22,31 @@ const MottoSection = () => {
       </div>
 
       <div
-        className="absoulte inset-0 h-full flex flex-col items-center justify-end gap-2 z-10"
+        className="absolute max-md:inset-0 md:bottom-11 md:right-12 h-full flex flex-col items-center md:items-start justify-end gap-2 z-10"
       >
         <h2
-            className="text-label-md"
+            className="text-label-md md:text-title-lg"
         >
             فناوری جدید، تجربه‌ای آشنا
         </h2>
 
+        {/* Mobile View Button */}
         <Button
+          className="md:hidden"
           variant="outline"
           size="sm"
         >
             بیشتر بدانید
             <ArrowLeftIcon className="w-6 h-6"/>
+        </Button>
+
+        {/* Mobile View Button */}
+        <Button
+          className="hidden md:block"
+          variant="outline"
+          size="md"
+        >
+            بیشتر بدانید
         </Button>
       </div>
     </Container>
